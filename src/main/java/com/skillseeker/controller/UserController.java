97db64181dddada45407e8007c8c9e2560e131d9
jbulgaru <iosifbulgaru2000@gmail.com>
2024-08-03 19:22:14 +0100

    feat: Add User entity, repository, service, and controller

---
+++ src/main/java/com/skillseeker/controller/UserController.java
@@ -0,0 +1,60 @@
+package com.skillseeker.controller;
+
+import com.skillseeker.model.User;
+import com.skillseeker.servce.interfaces.IUserService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+
+    private final IUserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User savedUser = userService.saveUser(user);
+        return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        Optional<User> user = userService.findUserById(id);
+        return user.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+
+    @GetMapping("/username/{username}")
+    public ResponseEntity<User> getUserByUsername(@PathVariable String username) {
+        Optional<User> user = userService.findUserByUsername(username);
+        return user.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+
+    @GetMapping("/email/{email}")
+    public ResponseEntity<User> getUserByEmail(@PathVariable String email) {
+        Optional<User> user = userService.findUserByEmail(email);
+        return user.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userService.findAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUserById(@PathVariable Long id) {
+        userService.deleteUserById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    //to-do Additional endpoints
+}
---
+++ src/main/java/com/skillseeker/servce/interfaces/IUserService.java
@@ -0,0 +1,17 @@
+package com.skillseeker.servce.interfaces;
+
+import com.skillseeker.model.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IUserService {
+    List<User> findAllUsers();
+    User saveUser(User user);
+    Optional<User> findUserById(Long id);
+    Optional<User> findUserByUsername(String username);
+    Optional<User> findUserByEmail(String email);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
+    void deleteUserById(Long id);
+}
