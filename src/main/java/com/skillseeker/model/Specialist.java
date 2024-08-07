c7ed6aa2751eab4477ab5c5570f0bd0df375b693
jbulgaru <iosifbulgaru2000@gmail.com>
2024-08-07 23:44:40 +0100

    add docker compose file

--- src/main/java/com/skillseeker/model/Specialist.java
+++ src/main/java/com/skillseeker/model/Specialist.java
@@ -1,17 +1,10 @@
 package com.skillseeker.model;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.SequenceGenerator;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
@@ -28,10 +21,10 @@ public class Specialist {
     @SequenceGenerator(name = "specialists_id_seq", sequenceName = "specialists_id_seq")
     private Long id;
 
-    @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(table = "users", name = "user_id")
-    @Column( name = "user_id", nullable = false)
-    private Long userId;
+
+    @OneToOne
+    @PrimaryKeyJoinColumn
+    private User user;
 
     @Column(name = "profile_photo", length = 255)
     private String profilePhoto;
--- src/main/java/com/skillseeker/model/User.java
+++ src/main/java/com/skillseeker/model/User.java
@@ -1,6 +1,10 @@
 package com.skillseeker.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
