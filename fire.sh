142dacd9206b657d28e4bd9a13565d8c94990d6e
jbulgaru <iosifbulgaru2000@gmail.com>
2025-01-24 16:27:06 +0000

    add .env if missed

--- fire.sh
+++ fire.sh
@@ -6,5 +6,10 @@ detach_terminal_flag=$2
 ${compose_command:="up"}
 ${detached_compose:=""}
 
+if [ ! -s docker/.env ]; then
+  echo  ".env file does not exist"
+  cp docker/.env.example docker/.env
+  echo ".env file created!"
+fi
 echo "--------------------------------$compose_command docker compose---------------------------------" && \
 (docker compose -f ./docker/compose.yml $compose_command $detach_terminal_flag )
